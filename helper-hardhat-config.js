84614883bde590ba0a85ab862cf43aac91ff1001
Rahees <ahdrahees.dev@gmail.com>
2023-02-27 21:03:01 +0530

    updated

---
+++ helper-hardhat-config.js
@@ -0,0 +1,16 @@
+const networkConfig = {
+    // created a networkConfig object for different network to use diffrent contract address of priceFeed in different network
+    5: {
+        name: "goerli",
+        ethUsdPriceFeed: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
+    },
+    137: {
+        name: "polygon mainnet",
+        ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
+    },
+    //31337
+}
+
+module.exports = {
+    networkConfig,
+}
