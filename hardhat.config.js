2c0fce5caf6fce893a39018abd8caf34f18da073
Rahees <ahdrahees.dev@gmail.com>
2023-02-27 20:20:14 +0530

    Added dotenv

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("hardhat-deploy")
+require("dotenv").config()
 
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL ||
+    "https://eth-mainnet.g.alchemy.com/v2/your-api-key"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0xkey"
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+    },
     solidity: "0.8.17",
     namedAccounts: {
         deployer: {
