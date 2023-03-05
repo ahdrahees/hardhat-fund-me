5c4dac064f2325a076032195ed4096100b1fe1a0
Rahees <ahdrahees.dev@gmail.com>
2023-03-05 13:05:22 +0530

    modified

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -8,16 +8,18 @@ const {
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-    const chianId = network.config.chainId
 
-    if (developmentChains.includes(chainId)) {
+    if (developmentChains.includes(network.name)) {
         log("Local network is detected! Deploying mocks...")
 
         await deploy("MockV3Aggregator", {
             contract: "MockV3Aggregator",
             from: deployer,
             log: true,
-            args: [],
+            args: [DECIMAL, INITIAL_ANSWER],
         })
+        log("Mocks deployed!")
+        log("----#----#----#----#----#----#----#----#----#----#----#----#----")
     }
 }
+module.exports.tags = ["all", "mocks"]
