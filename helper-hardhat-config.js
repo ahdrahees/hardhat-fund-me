ad9ac12b84e8a961e2b4808c3a415fcd43783fb3
Rahees <ahdrahees.dev@gmail.com>
2023-03-05 13:06:24 +0530

    modified

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -9,11 +9,11 @@ const networkConfig = {
         ethUsdPriceFeed: "0xF9680D99D6C9589e2a93a78A04A279e509205945",
     },
     //31337
-    const developmentChains = ["hardhat", "localhost"]
-
-    const DECIMAL = 8
-    const INITIAL_ANSWER = 200000000000
 }
+const developmentChains = ["hardhat", "localhost", "ganache"]
+
+const DECIMAL = 8
+const INITIAL_ANSWER = 200000000000
 
 module.exports = {
     networkConfig,
