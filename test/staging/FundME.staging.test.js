f8cdc95895ba6797226e94a0d5ece7ace4aa1711
Rahees <ahdrahees.dev@gmail.com>
2023-03-08 23:55:54 +0530

    modified

--- test/staging/FundME.staging.test.js
+++ test/staging/FundME.staging.test.js
@@ -1,1 +1,36 @@
+const { getNamedAccounts, deployment, ethers, network } = require("hardhat")
 
+const { developmentChains } = require("../../helper-hardhat-config")
+const { assert } = require("chai")
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("FundMe", async function () {
+          //ternary operator is used
+          let fundMe
+          let deployer
+          const sendValue = await ethers.utils.parseUnit("0.05")
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer
+              fundMe = await ethers.getContract("FundMe", deployer)
+          })
+
+          it("allow people to fund and withdraw", async function () {
+              const fundTxnResponse = await fundMe.fund({
+                  value: sendValue,
+              })
+              await fundTxnResponse.wait(1)
+
+              const withdrawTxnResponse = await fundMe.withdraw()
+              await withdrawTxnResponse.wait(1)
+
+              const endingFundMeBalance = await fundMe.provider.getBalance(
+                  fundMe.address
+              )
+              console.log(
+                  endingFundMeBalance.toString() +
+                      " should equal 0, running assert equal..."
+              )
+              assert.equal(endingFundMeBalance.toString(), "0")
+          })
+      })
