014d98442f6866ca3d874fd4324ccf555c5e626c
Rahees <ahdrahees.dev@gmail.com>
2023-03-09 13:59:27 +0530

    modified commit

--- scripts/fund.js
+++ scripts/fund.js
@@ -4,6 +4,7 @@ async function main() {
     const { deployer } = await getNamedAccounts()
     const fundMe = await ethers.getContract("FundMe", deployer)
     console.log("Funding contract...")
+
     const transactionResponse = await fundMe.fund({
         value: ethers.utils.parseEther("0.1"),
     })
